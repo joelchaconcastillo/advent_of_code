3935a9eada98006cc17077c118c711f6de30fa33
joel chacon <joel.chacon@oracle.com>
2024-12-12 11:04:00 -0600

    problem 3

---
+++ 2024/day-3/main.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool isNumber(string &st){
+   if(st.empty())return false;
+   for(auto i:st)if(!isdigit(i)) return false;
+   return true;
+}
+int main(){
+   string st, text="";
+   while(cin >> st){
+	text += st;
+   }
+   long long total_sum = 0;
+   int i = 0;
+   while(i<text.size()){
+        if( "mul(" == text.substr(i, 4)){
+	   int j = i+4;
+	   string num1 = "", num2 = "";
+	   while( j < text.size() && isdigit(text[j])){
+		   num1 += text[j++];
+	   }
+	   if(j<text.size() && text[j]!=','){
+		   i++; continue;
+	   }
+	   j++;
+	   while( j < text.size() && isdigit(text[j])){
+		   num2 += text[j++];
+	   }
+	   if(isNumber(num1) && isNumber(num2) && j<text.size() && text[j]==')'){
+		   total_sum += stoi(num1)*stoi(num2);
+		   i=j;
+	   }
+	}
+	i++;
+   }
+   cout << total_sum <<endl;
+   return 0;
+}
