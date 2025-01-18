24797b97c8405a4ce95aa605626bc8b97df60ca0
joel chacon <joel.chacon@oracle.com>
2025-01-18 12:15:15 -0600

    day 5

--- 2024/day-6/main.cpp
+++ 2024/day-6/main.cpp
@@ -1,14 +1,51 @@
 #include <bits/stdc++.h>
 using namespace std;
 int n, m;
+vector<string> grid;
+void _print(vector<string> &_grid){
+   for(auto line:_grid){
+       cout << line << endl;
+   }
+   cout <<endl;
+}
 bool feasible(pair<int, int> &pos, vector<string> &grid){
    return (pos.first >=0 
 		      && pos.first <n
 		      && pos.second >=0
 		      && pos.second <m);
 }
+bool hasExit(pair<int, int> current){
+   auto marked=grid;
+   int steps = 0;
+   while(steps<n*m){
+      //up
+      while(feasible(current, grid) 
+		      && grid[current.first][current.second]=='.')
+	      marked[current.first--][current.second]='X', steps++;
+      if(!feasible(current, grid)) return false;
+      if(grid[current.first][current.second]=='#') current.first++;
+      //right
+      while(feasible(current, grid)
+		      && grid[current.first][current.second]=='.')
+	      marked[current.first][current.second++]='X', steps++;
+      if(!feasible(current, grid)) return false;
+      if(grid[current.first][current.second]=='#') current.second--;
+      //down
+      while(feasible(current, grid)
+		      && grid[current.first][current.second]=='.')
+	      marked[current.first++][current.second]='X', steps++;
+      if(!feasible(current, grid)) return false;
+      if(grid[current.first][current.second]=='#') current.first--;
+      //left
+      while(feasible(current, grid)
+		      && grid[current.first][current.second]=='.')
+		      marked[current.first][current.second--]='X', steps++;
+      if(!feasible(current, grid)) return false;
+      if(grid[current.first][current.second]=='#') current.second++;
+   }
+   return true;
+}
 int main(){
-   vector<string> grid;
    string line;
    while(cin>>line) grid.push_back(line);
    n = grid.size(), m = grid.front().size();
@@ -18,38 +55,17 @@ int main(){
 	  if(grid[i][j] == '^') current={i, j};
       }
    }
-   cout << n<<" "<<m<<endl;
-   return 0;
    grid[current.first][current.second]='.';
-   auto marked=grid;
-   while(true){
-      //up
-      while(feasible(current, grid) 
-		      && grid[current.first][current.second]=='.')
-	      marked[current.first--][current.second]='X';
-      if(!feasible(current, grid))break;
-      if(grid[current.first][current.second]=='#') current.first++;
-      //right
-      while(feasible(current, grid)
-		      && grid[current.first][current.second]=='.')
-	      marked[current.first][current.second++]='X';
-      if(!feasible(current, grid))break;
-      if(grid[current.first][current.second]=='#') current.second--;
-      //down
-      while(feasible(current, grid)
-		      && grid[current.first][current.second]=='.')
-	      marked[current.first++][current.second]='X';
-      if(!feasible(current, grid))break;
-      if(grid[current.first][current.second]=='#') current.first--;
-      //left
-      while(feasible(current, grid)
-		      && grid[current.first][current.second]=='.')
-		      marked[current.first][current.second--]='X';
-      if(!feasible(current, grid))break;
-      if(grid[current.first][current.second]=='#') current.second++;
-   }
    int res=0;
-   for(auto i:marked)for(auto j:i)res+=j=='X';
+   for(int i = 0 ; i < n; i++){
+      for(int j = 0; j < m; j++){
+	  if(grid[i][j]=='#')continue;
+	  grid[i][j]='#';
+	  res += hasExit(current);
+//	  _print(grid);
+	  grid[i][j]='.';
+      }
+   }
    cout <<res<<endl;
    return 0;
 }
